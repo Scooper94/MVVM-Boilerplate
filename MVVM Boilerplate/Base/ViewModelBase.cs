cb7520f4e666a69b45a4eabb65dffe5605e1252f
Sam Cooper <samcooper100@msn.com>
2024-01-13 07:10:02 +0000

    Added View Model Base Class

---
+++ MVVM Boilerplate/Base/ViewModelBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Boilerplate.Base
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        protected void OnPropertyChanged()
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(null));
+            }
+        }
+    }
+}
--- MVVM Boilerplate/Class1.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace MVVM_Boilerplate
-{
-    public class Class1
-    {
-    }
-}
