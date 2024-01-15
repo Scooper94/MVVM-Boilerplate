850c66b9b5e39fab9ec3cf135b689ef8f014a921
Sam Cooper <samcooper100@msn.com>
2024-01-15 21:18:35 +0000

    Basic navigation service class added

---
+++ MVVM Boilerplate/Navigation/NavigationService.cs
@@ -0,0 +1,32 @@
+﻿using MVVM_Boilerplate.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MVVM_Boilerplate.Navigation
+{
+    public class NavigationService<TViewModel> : INavigationService where TViewModel : ViewModelBase
+    {
+        private readonly NavigationStore _navigationStore;
+        private readonly Func<TViewModel> _createViewModel;
+
+        public NavigationService(NavigationStore navigationStore, Func<TViewModel> createViewModel)
+        {
+            _createViewModel = createViewModel;
+            _navigationStore = navigationStore;
+        }
+
+        public void Navigate()
+        {
+            _navigationStore.CurrentViewModel = _createViewModel();
+        }
+
+
+
+
+    }
+}
---
+++ MVVM Boilerplate/Navigation/NavigationStore.cs
@@ -0,0 +1,33 @@
+﻿using MVVM_Boilerplate.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Boilerplate.Navigation
+{
+    public class NavigationStore
+    {
+        private ViewModelBase _currentViewModel;
+        public ViewModelBase CurrentViewModel
+        {
+            get => _currentViewModel;
+            set
+            {
+                if (value == _currentViewModel)
+                    return;
+                _currentViewModel = value;
+                OnViewModelChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler? ViewModelChangedChanged;
+
+        private void OnViewModelChanged()
+        {
+            ViewModelChangedChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentViewModel)));
+        }
+    }
+}
